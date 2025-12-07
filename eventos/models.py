9a57dae1b9f9f7d9d50689c63a0f76af17fcef6f
Sebastian Villarroel <svillarroel09@gmail.com>
2025-12-07 20:21:48 -0300

    refactor(eventos): implementar nomenclatura en imágenes y aplicar correcciones menores

--- eventos/models.py
+++ eventos/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from decimal import Decimal
 
+def ruta_temp_evento(instance, filename):
+    return f"eventos/temp_{filename}"
+
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=120)
@@ -30,7 +33,7 @@ class Evento(models.Model):
     forma_recinto = models.CharField(max_length=20, choices=FORMAS, default="cuadrado")
 
     categoria = models.ForeignKey("Categoria", null=True, blank=True, on_delete=models.SET_NULL)
-    imagen = models.ImageField(upload_to='eventos/', null=True, blank=True)
+    imagen = models.ImageField(upload_to=ruta_temp_evento, null=True, blank=True)
 
     precio_vip = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("300.00"))
     precio_avanzado = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("180.00"))
--- eventos/signals.py
+++ eventos/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_delete, pre_save
+from django.db.models.signals import post_delete, pre_save, post_save
 from django.dispatch import receiver
 from .models import Evento
+import os
 
 @receiver(post_delete, sender=Evento)
 def eliminar_imagen_evento(sender, instance, **kwargs):
@@ -20,3 +21,21 @@ def reemplazar_imagen_anterior(sender, instance, **kwargs):
     imagen_nueva = instance.imagen
     if imagen_anterior and imagen_anterior != imagen_nueva:
         imagen_anterior.delete(save=False)
+
+@receiver(post_save, sender=Evento)
+def renombrar_imagen_evento(sender, instance, created, **kwargs):
+    if instance.imagen:
+        path_original = instance.imagen.path
+        dir_base = os.path.dirname(path_original)
+
+        # Nueva ruta final
+        extension = instance.imagen.name.split('.')[-1]
+        nombre_final = f"evento_{instance.id}.{extension}"
+        nueva_ruta = os.path.join(dir_base, nombre_final)
+
+        if not os.path.exists(nueva_ruta):
+            os.rename(path_original, nueva_ruta)
+
+            # actualizar el campo en DB
+            instance.imagen.name = f"eventos/{nombre_final}"
+            instance.save()
